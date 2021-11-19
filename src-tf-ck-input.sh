755ed76f10e2489cf0ef2cb0e176172fd168325f
neofright <68615872+neofright@users.noreply.github.com>
2021-11-19 12:37:15 +0000

    Discover insensitive case flac file extensions
    Previously files must have had the extension ".flac" where as adding grep -i for case insensitive matches will allow ".FLAC" etc.
    
    $total_flac is an unused variable. Comment it out.

--- src-tf-ck-input.sh
+++ src-tf-ck-input.sh
@@ -35,10 +35,8 @@ until [[ $valid_dir == "YES" ]]; do
 		exit;;
 esac
 
-	rp_input_flac_dir=$( realpath -Pq $input_flac_dir 2>/dev/null )
-
-	flac_count=$( find $rp_input_flac_dir 2>/dev/null | grep -c "\.flac" )
-	total_flac=$flac_count
+	flac_count="$( find "$rp_input_flac_dir" 2>/dev/null | grep -ic "\.flac" )"
+	#total_flac="$flac_count"
 
 	if [[ -d $rp_input_flac_dir && $flac_count -gt 0 ]];
 	then
