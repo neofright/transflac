7f390eae15945953eaf9a4d371e083adda80e15d
neofright <68615872+neofright@users.noreply.github.com>
2021-12-24 09:37:04 +0000

    Follow symlinks
    Use find -L to follow symlinks.
    
    Previously transflac would fail with "Invalid FLAC Directory" when attempting to transcode a symlinked directory tree.

--- src-tf-ck-input.sh
+++ src-tf-ck-input.sh
@@ -36,7 +36,7 @@ until [[ "$valid_dir" == "YES" ]]; do
 esac
 	rp_input_flac_dir="$( realpath -Pq "$input_flac_dir" 2>/dev/null )"
 
-	flac_count="$( find "$rp_input_flac_dir" 2>/dev/null | grep -ic "\.flac" )"
+	flac_count="$( find -L "$rp_input_flac_dir" -mindepth 1 -type f -iname "*.flac" 2>/dev/null | wc -l | tr -d ' ')"
 	total_flac="$flac_count"
 
 	if [[ -d "$rp_input_flac_dir" && "$flac_count" -gt 0 ]];
